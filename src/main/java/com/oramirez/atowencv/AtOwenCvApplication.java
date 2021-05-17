b924009ee68e577f9020a946affbce15391616d7
RamOwen99 <owen.ramirez@agilethought.com>
2021-05-17 18:29:12 -0500

    Spring init

---
+++ src/main/java/com/oramirez/atowencv/AtOwenCvApplication.java
@@ -0,0 +1,13 @@
+package com.oramirez.atowencv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtOwenCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtOwenCvApplication.class, args);
+	}
+
+}
