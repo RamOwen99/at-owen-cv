09ca1f2de871295c9fbe384fb09e242820a669f7
RamOwen99 <owen.ramirez@agilethought.com>
2021-05-18 12:35:18 -0500

    Create models

---
+++ src/main/java/com/oramirez/atowencv/model/cv/Skills.java
@@ -0,0 +1,13 @@
+package com.oramirez.atowencv.model.cv;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Skills {
+    private String name;
+    private Byte percentage;
+}
