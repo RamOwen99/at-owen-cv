f5e68ce6c8881e16490f123c7d9e223c4d53e26f
RamOwen99 <owen.ramirez@agilethought.com>
2021-05-20 16:52:08 -0500

    Create config model
    This model will help us with the letters' font and color

---
+++ src/main/java/com/oramirez/atowencv/model/cv/Config.java
@@ -0,0 +1,13 @@
+package com.oramirez.atowencv.model.cv;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Config {
+    private String color;
+    private String fontFamily;
+    private String genericFamily;
+}
