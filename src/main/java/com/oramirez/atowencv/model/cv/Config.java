4425c158cea46a96c870d05bbef9b82d8c3bc3ce
RamOwen99 <owen.ramirez@agilethought.com>
2021-05-21 10:56:28 -0500

    Modify properties fields

---
+++ src/main/java/com/oramirez/atowencv/model/cv/Config.java
@@ -0,0 +1,12 @@
+package com.oramirez.atowencv.model.cv;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Config {
+    private String color;
+    private String fontFamily;
+    private String genericFamily;
+}
