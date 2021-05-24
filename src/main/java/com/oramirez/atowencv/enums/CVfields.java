7e8f292c2b22d4e474e8d39d6123e83c79624687
RamOwen99 <owen.ramirez@agilethought.com>
2021-05-24 18:32:54 -0500

    Implement validation methods
    Here I implement the validation methods to check the user's data.
    
    Also I make a enum to store the user's data fields like firstname, etc.

---
+++ src/main/java/com/oramirez/atowencv/enums/CVfields.java
@@ -0,0 +1,23 @@
+package com.oramirez.atowencv.enums;
+
+public enum CVfields {
+    FIRST_NAME("First name"),
+    LAST_NAME("Last Name"),
+    JOB("Job"),
+    CITY("City"),
+    COUNTRY("Country"),
+    EMAIL("Email"),
+    PHONE_NUMBER("Phone number"),
+    ;
+
+    private final String FIELD_NAME;
+
+    CVfields(String fieldName) {
+        this.FIELD_NAME = fieldName;
+    }
+
+    public String getFieldName() {
+        return this.FIELD_NAME;
+    }
+
+}
