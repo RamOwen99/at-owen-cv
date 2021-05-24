2e0668a5290aac0551080f8444feac1b6c8f4511
RamOwen99 <owen.ramirez@agilethought.com>
2021-05-24 16:34:01 -0500

    Validate personal information

--- src/main/java/com/oramirez/atowencv/exception/ExceptionMessages.java
+++ src/main/java/com/oramirez/atowencv/exception/ExceptionMessages.java
@@ -2,7 +2,12 @@ package com.oramirez.atowencv.exception;
 
 public enum ExceptionMessages {
     CV_BY_ID_NOT_FOUND("The CV with the id %s does not exist"),
-    CV_CAN_NOT_UPDATE("The cv with the id %s can not be update, because it doesn't exist");
+    CV_CAN_NOT_UPDATE("The cv with the id %s can not be update, because it doesn't exist"),
+
+    // Messages when a field is invalid
+    INVALID_FIELD("The field %s is not valid"),
+    INVALID_EMAIL_FORMAT("The %s does not have the correct format. Try with this format \"example@domain.com\""),
+    INVALID_PHONE_NUMBER("The %s is not valid because it is not a phone number");
 
     private final String MESSAGE_EXCEPTION;
 
